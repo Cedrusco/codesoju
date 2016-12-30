7e564ad648243db1a26873f257c5841993f86f79
Mohamed Maher <moh.maher@gmail.com>
2016-12-29 19:12:43 -0500

    disable angular 2 and refactor the directory copy.

--- generators/app/index.js
+++ generators/app/index.js
@@ -13,12 +13,26 @@ module.exports = yeoman.extend({
             prompts = [{
                 type: 'input',
                 name: 'name',
-                message: 'What is the name of your application?'
+                message: 'What is the name of your application?',
+                default : this.appname
             }, {
                 type: 'list',
                 name: 'angularVersion',
                 message: 'Choose what type of app you would like to buld:',
-                choices: ['Angular-1', 'Angular-2', 'Cordova-Mobile']
+                choices: [ // 'Angular-1', 'Angular-2', 'Cordova-Mobile'
+                    {
+                        name: 'Angular-1',
+                        value: 'Angular-1',
+                        checked: true
+                    }, {
+                        name: 'Angular-2 - comming soon',
+                        value: 'Angular-2',
+                        disabled: true
+                    }, {
+                        name: 'Cordova-Mobile',
+                        value: 'Cordova-Mobile'
+                    }
+                ]
             }];
 
         return this.prompt(prompts).then(function (props) {
@@ -36,13 +50,16 @@ module.exports = yeoman.extend({
         if (this.angularVersion === 'Angular-1') {
 
             this.fs.copy(this.templatePath('angular1'), this.destinationRoot());
+            this.fs.copyTpl(this.templatePath('angular1/**/*.{json,md,html}'), this.destinationRoot(), this);
+
 
         } else if (this.angularVersion === 'Angular-2') {
 
-            this.fs.copy(this.templatePath('angular2'), this.destinationRoot());
+            this.fs.copyTpl(this.templatePath('angular2'), this.destinationRoot(), this);
 
         } else {
-            this.fs.copy(this.templatePath('cordova-mobile'), this.destinationRoot());
+            this.fs.copy(this.templatePath('cordova-mobile'), this.destinationRoot(), this);
+            this.fs.copyTpl(this.templatePath('cordova-mobile/**/*.{json,md,html}'), this.destinationRoot(), this);
         }
 
     },
