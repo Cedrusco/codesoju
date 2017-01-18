717197da0eb20f1ea0fd805dd3fbfb2c444fec2e
Mohamed Maher <moh.maher@gmail.com>
2017-01-18 17:38:30 -0500

    Update: version 0.2.5

--- generators/app/templates/angular1/server/configuration/environment/development.js
+++ generators/app/templates/angular1/server/configuration/environment/development.js
@@ -3,6 +3,6 @@
 // Use this file for api variables. This file should not be tracked by git.
 
 module.exports = {
-    PORT: process.env.VCAP_APP_PORT || 1337,
+    PORT: process.env.VCAP_APP_PORT || process.env.PORT  || 1337,
     NYTAPI: 'f47b5dbbdee949f59acc93afdf017e37'
 };
